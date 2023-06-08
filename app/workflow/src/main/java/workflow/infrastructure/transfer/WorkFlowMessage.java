4851be8d6ab9aa68adb40124a84192760a6fc506
setoguchi <setoguchi.takuma@gmail.com>
2023-06-08 10:44:15 +0900

    不要コメント削除

--- app/workflow/src/main/java/workflow/infrastructure/transfer/WorkFlowMessage.java
+++ app/workflow/src/main/java/workflow/infrastructure/transfer/WorkFlowMessage.java
@@ -4,19 +4,25 @@ import workflow.domain.model.form.ApplicationFormId;
 import workflow.domain.model.user.UserId;
 import workflow.domain.model.workflow.WorkFlowEvent;
 
-public class WorkFlowMessage {
-
-    WorkFlowEvent workFlowEvent;
-    ApplicationFormId applicationFormId;
-    UserId assignedUserId;
-
-    WorkFlowMessage(WorkFlowEvent workFlowEvent, ApplicationFormId applicationFormId, UserId assignedUserId) {
-        this.workFlowEvent = workFlowEvent;
-        this.applicationFormId = applicationFormId;
-        this.assignedUserId = assignedUserId;
-    }
+public record WorkFlowMessage(
+    WorkFlowEvent workFlowEvent,
+    ApplicationFormId applicationFormId,
+    UserId assignedUserId
+) {
 
     public static WorkFlowMessage appliedMessage(ApplicationFormId applicationFormId, UserId assignedUserId) {
         return new WorkFlowMessage(WorkFlowEvent.申請, applicationFormId, assignedUserId);
+    }
+
+    public static WorkFlowMessage approvedMessage(ApplicationFormId applicationFormId, UserId assignedUserId) {
+        return new WorkFlowMessage(WorkFlowEvent.承認, applicationFormId, assignedUserId);
+    }
+
+    public static WorkFlowMessage rejectedMessage(ApplicationFormId applicationFormId, UserId assignedUserId) {
+        return new WorkFlowMessage(WorkFlowEvent.差戻し, applicationFormId, assignedUserId);
+    }
+
+    public static WorkFlowMessage canceledMessage(ApplicationFormId applicationFormId, UserId assignedUserId) {
+        return new WorkFlowMessage(WorkFlowEvent.却下, applicationFormId, assignedUserId);
     }
 }
--- app/workflow/src/main/java/workflow/infrastructure/transfer/WorkFlowPublisher.java
+++ app/workflow/src/main/java/workflow/infrastructure/transfer/WorkFlowPublisher.java
@@ -19,31 +19,24 @@ public class WorkFlowPublisher implements WorkFlowTransfer {
         this.rabbitTemplate = rabbitTemplate;
     }
 
-//    public void instruct(InstructionNumber instructionNumber) {
-//        logger.info("publish instructionNumber={}", instructionNumber);
-//        rabbitTemplate.convertAndSend("md-inbound-instruction.md", instructionNumber.toString());
-//    }
-
-
 
     @Override
     public void notifyApplied(Work work) {
-//        logger.info("申請（申請ID: {}) が ユーザー(ユーザーID {}) 申請されました", work.applicationFormId(), work.applicantUserId());
         rabbitTemplate.convertAndSend("work-flow-topic", "",  WorkFlowMessage.appliedMessage(work.applicationFormId(), work.assignedUserId()));
     }
 
     @Override
     public void notifyApproved(ApplicationFormId applicationFormId, User applicantUser) {
-        logger.info("ユーザー(ユーザーID {})の申請（申請ID: {}) が 承認されました", applicationFormId, applicantUser.userId());
+        rabbitTemplate.convertAndSend("work-flow-topic", "",  WorkFlowMessage.approvedMessage(applicationFormId, applicantUser.userId()));
     }
 
     @Override
     public void notifyRejected(Work work, User applicantUser) {
-        logger.info("ユーザー(ユーザーID {})の申請（申請ID: {}) が 差し戻されました", work.applicationFormId(), applicantUser.userId());
+        rabbitTemplate.convertAndSend("work-flow-topic", "",  WorkFlowMessage.rejectedMessage(work.applicationFormId(), applicantUser.userId()));
     }
 
     @Override
     public void notifyCanceled(ApplicationFormId applicationFormId, User applicantUser) {
-        logger.info("ユーザー(ユーザーID {})の申請（申請ID: {}) が 却下されました", applicationFormId, applicantUser.userId());
+        rabbitTemplate.convertAndSend("work-flow-topic", "",  WorkFlowMessage.canceledMessage(applicationFormId, applicantUser.userId()));
     }
 }
